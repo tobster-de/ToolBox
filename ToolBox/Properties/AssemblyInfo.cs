4c17bae29cfba1f2c785f9da815dff1efa0d6745
Tobias Gaertner <tgaertner@gmx.net>
2021-04-11 09:18:46 +0200

    move project to sub directory

--- ToolBox/Properties/AssemblyInfo.cs
+++ ToolBox/Properties/AssemblyInfo.cs
@@ -35,6 +35,3 @@ using System.Security.Permissions;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, Execution = true)]
-[assembly: PermissionSet(SecurityAction.RequestOptional, Name = "Nothing")]
